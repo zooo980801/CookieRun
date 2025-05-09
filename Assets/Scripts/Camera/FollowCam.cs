7065dc9c1b040c1dd4ed0c7dab80c2e2b0d78b04
Leekiahn <kiahn2000@gmail.com>
2025-05-09 17:48:42 +0900

    update : FollowCam.cs 추가

---
+++ Assets/Scripts/Camera/FollowCam.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float camHeight = 0f;
+    
+    private Vector3 playerPos;
+    private Vector3 camOffset;
+    
+    private void Awake()
+    {
+        if (playerTransform == null)
+        {
+            Debug.LogError("playerTransform이 없습니다.");
+        }
+        
+        camOffset = new Vector3(5f, 0f, -10f);
+    }
+    
+    void Update()
+    {
+        playerPos = new Vector3(playerTransform.position.x, camHeight, 0f);
+        transform.position = playerPos + camOffset;
+    }
+}
