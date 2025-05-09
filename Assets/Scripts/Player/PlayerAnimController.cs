9d840afd1ff48dee8e0f5d38eaedee1f9d0b913f
Leekiahn <kiahn2000@gmail.com>
2025-05-09 11:14:16 +0900

    fix : 애니메이션 버그 수정, Unit.cs 추상클래스로 수정

--- Assets/Scripts/Player/PlayerAnimController.cs
+++ Assets/Scripts/Player/PlayerAnimController.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private BoxCollider2D collider;
+
+    private Vector2 currentColliderSize;
+    private Vector2 currentColliderOffset;
+    private Vector2 colliderSize = new Vector2(1, 0.6f);
+    private Vector2 colliderOffset = new Vector2(0.4f, -0.6f);
+
+    private void Awake()
     {
+        animator = GetComponentInChildren<Animator>();
+        if (animator == null) { Debug.LogError("Animator가 없습니다."); }
         
+        collider = GetComponent<BoxCollider2D>();
+        if (collider == null) { Debug.LogError("BoxCollider2D가 없습니다."); }
+        
+        currentColliderSize = collider.size;
+        currentColliderOffset = collider.offset;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SlideAnim(bool pressedControl)
     {
-        
+        switch (pressedControl)
+        {
+            case true:
+                ColliderCtrl(colliderSize, colliderOffset);
+                animator.SetBool("IsSlide", pressedControl);
+                break;
+            case false:
+                ColliderCtrl(currentColliderSize, currentColliderOffset);
+                animator.SetBool("IsSlide", pressedControl);
+                break;
+        }
+    }
+
+    public void JumpAnim(bool isGrounded)
+    {
+        animator.SetBool("IsGrounded", isGrounded);
+    }
+
+    private void ColliderCtrl(Vector2 _size, Vector2 _offset)
+    {
+        collider.size = _size;
+        collider.offset = _offset;
     }
 }
--- Assets/Scripts/Player/Unit.cs
+++ Assets/Scripts/Player/Unit.cs
@@ -3,93 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public abstract class Unit : MonoBehaviour
 {
     //최대 체력
-    private const float fullHP = 100f;
+    protected const float fullHP = 100f;
     
     //HP
-    [SerializeField] private float hp;
-    protected float Hp
-    {
-        get { return hp; }
-        set
-        {
-            hp = Mathf.Clamp(value, 0, fullHP);
-        }
-    }
-
-    [SerializeField] private float jumpForce;
-    protected float JumpForce
-    {
-        get { return jumpForce; }
-        set
-        {
-            jumpForce = Mathf.Max(0, value);
-        }
-    }
-
-    [SerializeField] private bool isGrounded = false;
-    private float groundRayLength = 1.2f;
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] protected float hp;
+    protected abstract float Hp { get; set; }
+    
+    //점프 파워
+    [SerializeField] protected float jumpForce;
+    protected abstract float JumpForce { get; set; }
+    
+    //지면에 있는지, 지면과의 거리, 지면 레이어마스크
+    [SerializeField] protected bool isGrounded = false;
+    [SerializeField] protected float groundRayLength = 1.2f;
+    [SerializeField] protected LayerMask groundLayer;
     
     protected Rigidbody2D rb;
-    [SerializeField] protected Animator animator;
-    protected BoxCollider2D collider;
 
-    private Vector2 currentColliderSize;
-    private Vector2 currentColliderOffset;
-    private Vector2 colliderSize = new Vector2(1, 0.6f);
-    private Vector2 colliderOffset = new Vector2(0.4f, -0.6f);
+    protected PlayerAnimController animCtrl;
 
     protected void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         if (rb == null) { Debug.LogError("Rigidbody2D가 없습니다.");}
 
-        animator = GetComponentInChildren<Animator>();
-        if (animator == null) { Debug.LogError("Animator가 없습니다.");}
-
-        collider = GetComponent<BoxCollider2D>();
-        if (collider == null) { Debug.LogError("BoxCollider2D가 없습니다."); }
-        
-        currentColliderSize = collider.size;
-        currentColliderOffset = collider.offset;
+        animCtrl = GetComponent<PlayerAnimController>();
+        if (animCtrl == null) { Debug.LogError("PlayerAnimController가 없습니다."); }
     }
 
-    protected void Jump()
+    protected void Update()
     {
-        if (rb != null)
-        {
-            //땅에 오브젝트가 닿았는지
-            isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundRayLength, groundLayer);
-            
-            //땅에 닿았을 때만 점프
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
-        }
+        Jump();
+        Slide();
     }
 
-    protected void Slide()
-    {
-        bool pressedControl = Input.GetKey(KeyCode.LeftControl);
-        
-        if (isGrounded)
-        {
-            if (pressedControl)
-            {
-                animator.SetBool("IsSlide", true);
-                collider.size = colliderSize;
-                collider.offset = colliderOffset;
-            }
-            else
-            {
-                animator.SetBool("IsSlide", false);
-                collider.size = currentColliderSize;
-                collider.offset = currentColliderOffset;
-            }
-        }
-    }
+    protected abstract void Jump();
+
+    protected abstract void Slide();
 }
