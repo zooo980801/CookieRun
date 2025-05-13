3ee58ef0a7f1e7a9cbe74afaa28eb8d415706ee4
lymsg <114712935+lymsg@users.noreply.github.com>
2025-05-13 10:03:49 +0900

    feat. 플레이어 파티클추가
    걷기 파티클

---
+++ Assets/Scripts/Effects/DustParticleControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustParticleControl : MonoBehaviour
+{
+    [SerializeField] private bool createDustOnWalk = true;
+    [SerializeField] private ParticleSystem dustParticleSystem;
+
+    public void CreateDustParticles()
+    {
+        if (createDustOnWalk)
+        {
+            dustParticleSystem.Stop();
+            dustParticleSystem.Play();
+        }
+    }
+}
