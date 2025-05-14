94eedcfa128018d7beb1918b9945d6c964a400fe
lymsg <114712935+lymsg@users.noreply.github.com>
2025-05-14 20:15:37 +0900

    fix.스포너 & 튜토리얼씬 픽스

--- Assets/Scripts/Item/Items/before/CoinItem.cs
+++ Assets/Scripts/Item/Items/before/CoinItem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CoinItem : Item
 {
@@ -9,12 +10,18 @@ public class CoinItem : Item
     [SerializeField]
     private SpriteRenderer _renderer;
     [SerializeField]
+
+    private TutorialManager tutorialManager;
+
     private GroundObjectData _data
     {
         get { return data; }
         set { data = value; }
     }
-
+    private void Awake()
+    {
+        tutorialManager = FindObjectOfType<TutorialManager>();
+    }
     public override void Initialize(GroundObjectData data)
     {
         this._data = data;
@@ -31,6 +38,16 @@ public class CoinItem : Item
         Debug.Log($"[CoinItem] Trigger with: {other.gameObject.name}");
         if (other.CompareTag("Player"))
         {
+            if (SceneManager.GetActiveScene().name == "TutorialScene") //현재 튜토리얼씬일때
+            {
+                Debug.Log("듀토씬진입");
+                if (tutorialManager != null && tutorialManager.IsCurrentStep(TutorialStep.CollectCoin) )
+                {
+                    Debug.Log("올라감");
+                    tutorialManager.AdvanceStep();
+                }
+                
+            }
             ApplyEffect();
             gameObject.SetActive(false);
         }
